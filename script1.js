c96977f77546c3de7a518ff90bb5f3c7ef355aa5
maikon014 <maikon014barbosa@gmail.com>
2024-08-06 00:19:08 -0300

    Styling CSS Version 03

---
+++ script1.js
@@ -0,0 +1,25 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) currentIndex = 0;
+    if (index < 0) currentIndex = slides.length - 1;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active');
+    }
+    slides[currentIndex].classList.add('active');
+}
+
+function nextSlide() {
+    currentIndex++;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex--;
+    showSlide(currentIndex);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex);
+});
